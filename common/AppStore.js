34605d4ef142181f5b99bfd4ef8397e500fb50c6
Namelos <namelos@outlook.com>
2017-08-20 01:28:45 +0800

    add routes

--- common/AppStore.js
+++ common/AppStore.js
@@ -1,9 +1,28 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
-export const AppStore = () => <View style={styles.center}>
-  <Text>Hello world!</Text>
-</View>
+import { NativeRouter, Link, Route } from 'react-router-native'
+
+const Home = () => <Text>Welcome to AppStore.</Text>
+const App1 = () => <Text>This is App1, a sub-project.</Text>
+
+export const AppStore = () => <NativeRouter>
+  <View style={styles.center}>
+    <View>
+      <Link to="/">
+        <Text>Home</Text>
+      </Link>
+      <Link to="/app1">
+        <Text>App1</Text>
+      </Link>
+    </View>
+
+    <View>
+      <Route exact path="/" component={Home}/>
+      <Route path="/app1" component={App1}/>
+    </View>
+  </View>
+</NativeRouter>
 
 const styles = StyleSheet.create({
   center: {
