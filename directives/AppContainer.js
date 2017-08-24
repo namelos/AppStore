ba75f2f03f59d1539c03aa771a311b757b545a5a
twttao <ttao@thoughtworks.com>
2017-08-24 23:25:56 +0800

    will first prompt user to download the project

--- directives/AppContainer.js
+++ directives/AppContainer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactNative from 'react-native'
-import { View } from 'react-native'
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native'
 import jsBundleProvider from '../services/js-bundle-provider'
 
 export default class AppContainer extends React.Component {
@@ -13,7 +13,7 @@ export default class AppContainer extends React.Component {
 		this.uri = uri
 	}
 
-	async componentWillMount() {
+	async _downloadAndRunBundle() {
 		const js = await jsBundleProvider.getByUri(this.uri)
 		const appRoot = eval(js)({ React, ReactNative })
 		this.setState({
@@ -27,6 +27,37 @@ export default class AppContainer extends React.Component {
 		if (App) {
 			return <App />
 		}
-		return <View />
+		return (
+			<View style={styles.container}>
+				<Text style={styles.prompt}>You have not downloaded this app yet.</Text>
+				<Text style={styles.prompt}>Do you want to download it now?</Text>
+				<TouchableOpacity
+					onPress={this._downloadAndRunBundle.bind(this)}
+					style={styles.download}>
+					<Text>Download</Text>
+				</TouchableOpacity>
+			</View>
+		)
 	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		width: '100%',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	prompt: {
+		width: 300,
+		textAlign: 'center',
+	},
+	download: {
+		marginTop: 20,
+		backgroundColor: 'whitesmoke',
+		width: 120,
+		height: 35,
+		alignItems: 'center',
+		justifyContent: 'center',
+	}
+})
