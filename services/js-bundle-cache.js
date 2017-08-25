9ed5c325420c298a8049bab4b3136593356d2fc0
twttao <ttao@thoughtworks.com>
2017-08-25 08:21:22 +0800

    fix read from cache issue

--- services/js-bundle-cache.js
+++ services/js-bundle-cache.js
@@ -5,17 +5,21 @@ const client = RNFetchBlob.config({fileCache: true})
 
 export default {
   async fetch(uri) {
-    const cached = await this._tryReadFromCache(uri)
-    if(cached) {
-      return cached
+    if(await this.isCached(uri)) {
+      const cached = await this._tryReadFromCache(uri)
+      if(cached) {
+        return cached
+      }
     }
     return this._downloadAndRead(uri)
   },
   async _tryReadFromCache(uri) {
     const path = await _getBundlePath(uri)
+    let cached
     try {
-      return _readBundle(path)
+      cached = await _readBundle(path)
     } catch(e) {}
+    return cached
   },
   async _downloadAndRead(uri) {
     const res = await client.fetch('GET', uri)
@@ -36,7 +40,7 @@ export default {
 }
 
 const _getBundlePath = async uri => AsyncStorage.getItem(uri)
-const _saveBundlePath = async (uri, path) => await AsyncStorage.setItem(uri, path)
-const _clearBundlePath = async uri => await AsyncStorage.clear(uri)
-const _readBundle = async path => await RNFetchBlob.fs.readFile(path, 'utf8')
-const _deleteBundle = async path => await RNFetchBlob.fs.unlink(path)
+const _saveBundlePath = async (uri, path) => AsyncStorage.setItem(uri, path)
+const _clearBundlePath = async uri => AsyncStorage.clear(uri)
+const _readBundle = async path => RNFetchBlob.fs.readFile(path, 'utf8')
+const _deleteBundle = async path => RNFetchBlob.fs.unlink(path)
